929ff3df32ac60f4f27eb66c3c389d905eb3b4ee
Matheus-Janczkowski <matheusj2009@hotmail.com>
2025-12-08 14:25:40 -0300

    adds the incompressibility tests

---
+++ source/Grante/MultiMech/aa_tests/hyperelasticity/disc_neo_hookean_incompressible.py
@@ -0,0 +1,144 @@
+# Routine to test a hyperelastic disc
+
+import os
+
+from ....PythonicUtilities.path_tools import get_parent_path_of_file
+
+from ....MultiMech.constitutive_models.hyperelasticity import isotropic_hyperelasticity as constitutive_models
+
+from ....MultiMech.physics import hyperelastic_incompressible_cauchy_continuum as variational_framework
+
+########################################################################
+########################################################################
+##                      User defined parameters                       ##
+########################################################################
+########################################################################
+
+########################################################################
+#                          Simulation results                          #
+########################################################################
+
+# Defines the path to the results directory 
+
+results_path = get_parent_path_of_file() #os.getcwd()+"//aa_tests//hyperelasticity//results"
+
+displacement_fileName = "displacement.xdmf"
+
+post_processes = dict()
+
+post_processes["SaveField"] = {"directory path":results_path, 
+"file name":displacement_fileName}
+
+########################################################################
+#                         Material properties                          #
+########################################################################
+
+# Sets the Young modulus and the Poisson ratio
+
+E = 1E6
+
+v = 0.3
+
+# Sets a dictionary of properties
+
+material_properties = dict()
+
+material_properties["E"] = E
+
+material_properties["nu"] = v
+
+# Sets the material as a neo-hookean material using the corresponding
+# class
+
+constitutive_model = constitutive_models.Neo_Hookean(material_properties)
+
+########################################################################
+#                                 Mesh                                 #
+########################################################################
+
+# Defines the name of the file to save the mesh in. Do not write the fi-
+# le termination, e.g. .msh or .xdmf; both options will be saved automa-
+# tically
+
+mesh_fileName = {"length x": 1.0, "length y": 1.5, "length z": 5.0, "n"+
+"umber of divisions in x": 3, "number of divisions in y": 5, "number o"+
+"f divisions in z": 10}#"tests//test_meshes//intervertebral_disc"
+
+########################################################################
+#                            Function space                            #
+########################################################################
+
+# Defines the shape functions degree
+
+polynomial_degree = 2
+
+########################################################################
+#                           Solver parameters                          #
+########################################################################
+
+# Sets the solver parameters in a dictionary
+
+solver_parameters = dict()
+
+solver_parameters["newton_relative_tolerance"] = 1e-4
+
+solver_parameters["newton_absolute_tolerance"] = 1e-4
+
+solver_parameters["newton_maximum_iterations"] = 10
+
+# Sets the initial time
+
+t = 0.0
+
+# Sets the final pseudotime of the simulation
+
+t_final = 1.0
+
+# Sets the maximum number of steps of loading
+
+maximum_loadingSteps = 5
+
+########################################################################
+#                          Boundary conditions                         #
+########################################################################
+
+# Defines a load expression
+
+maximum_load = 2E4
+
+# Assemble the traction vector using this load expression
+
+traction_boundary = {"load case": "UniformReferentialTraction", "ampli"+
+"tude_tractionX": 0.0, "amplitude_tractionY": 0.0, "amplitude_tractionZ": 
+maximum_load, "parametric_load_curve": "square root", "t": t, "t_final":
+t_final}
+
+# Defines a dictionary of tractions
+
+traction_dictionary = dict()
+
+traction_dictionary["top"] = traction_boundary
+
+# Defines a dictionary of boundary conditions. Each key is a physical
+# group and each value is another dictionary or a list of dictionaries 
+# with the boundary conditions' information. Each of these dictionaries
+# must have the key "BC case", which carries the name of the function 
+# that builds this boundary condition
+
+bcs_dictionary = dict()
+
+bcs_dictionary["bottom"] = {"BC case": "FixedSupportDirichletBC"}
+
+########################################################################
+########################################################################
+##                      Calculation and solution                      ##
+########################################################################
+########################################################################
+
+# Solves the variational problem
+
+variational_framework.hyperelasticity_two_fields(
+constitutive_model, traction_dictionary, maximum_loadingSteps, t_final, 
+post_processes, mesh_fileName, solver_parameters, 
+polynomial_degree_displacement=polynomial_degree, t=t, 
+dirichlet_boundaryConditions=bcs_dictionary, verbose=True)
